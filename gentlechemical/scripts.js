51b036f78694227ea634fab66027da4477327a0f
studentpenguincoding <student@penguincodingschool.com>
2016-12-03 12:02:10 -0500

    Add files via upload

---
+++ gentlechemical/scripts.js
@@ -0,0 +1,12 @@
+function askQuestions() {
+alert("Hello my name is Bora");
+var name=prompt("What is your name");
+confirm("Nice to meet you "+ name);
+
+var age = prompt("How old are you?");
+if (age >18) {
+  alert ("=You are too old to play thisgame");
+} else {
+confirm("Let's start this game "+ name);
+}
+}
