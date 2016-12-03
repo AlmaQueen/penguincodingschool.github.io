0d39b68910cc5c618b58adaa53b577794047591b
studentpenguincoding <student@penguincodingschool.com>
2016-12-03 15:59:38 -0500

    Add files via upload

---
+++ violetorder/scripts.js
@@ -0,0 +1,12 @@
+function askQuestions() {
+  alert ("Im a very important news reporter,trust me :)");
+  var answer = prompt('Do you think ducks will rule the world some day? Yes or no?');
+  if(answer==="Yes" || answer==="yes") {
+    alert("Ducks are cool and so are you :)");
+  } else if (answer==="No" || answer==="no") {
+    alert("They will you will see and then you will realize that ducks ARE amazing and WILL rule the earth :)");
+  } else {
+    alert("I didn't get that, yes or no please.");
+    
+  }
+}
