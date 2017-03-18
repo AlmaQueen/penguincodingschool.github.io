ed1bb13b12e9f6ebb83e1a78b55eb32858727cf7
studentpenguincoding <student@penguincodingschool.com>
2017-03-18 10:05:12 -0400

    Add files via upload

---
+++ penguin6/quiz.js
@@ -0,0 +1,104 @@
+ 
+(function() {
+   var questions = [{
+  question: "What is the fear of snow called?",
+  choices:["chionophobia","snowphobia","claustrophobia","icephobia"],
+  answer:0},
+  {question:"What is the fear of heights called?",
+   choices:["heightophobia","claustrophobia","acrophobia","arachnophobia"],
+   answer:2},
+   {question:"What is the fear of spiders called?",
+   choices:["arachnophobia","acrophobia","phobiaphobia","spiderphobia" ],
+   answer:0},
+   {question:"What is the fear of fears called?",
+   choices:["fearophobia","philosophobia","phobiaphobia"],
+   answer:2},
+  {question:"What's the fear of animals called?",
+   choices:["zelophobia","zemmiphobia","zoophobia"],
+   answer:2}
+  ];
+  
+  var counter = 0;
+  var selections = [];
+  var quiz = $('#quiz');
+  
+  displayNext();
+  
+  $('#next').on('click', function (e) {
+    choose();
+    counter++;
+    displayNext();
+})
+
+
+function createQuestion(index) {
+    var qElement = $('<div>', {
+      id: 'question'
+    });
+    
+    var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+    qElement.append(header);
+    
+    var question = $('<p>').append(questions[index].question);
+    qElement.append(question);
+    
+    var radioButtons = createRadios(index);
+    qElement.append(radioButtons);
+    
+    return qElement;
+  }
+  
+  function createRadios(index) {
+    var radioList = $('<ul>');
+    var item;
+    var input = '';
+    for (var i = 0; i < questions[index].choices.length; i++) {
+      item = $('<li>');
+      input = '<input type="radio" name="answer" value=' + i + ' />';
+      input += questions[index].choices[i];
+      item.append(input);
+      radioList.append(item);
+    }
+    return radioList;
+  }
+  
+  function choose() {
+    selections[counter] = +$('input[name="answer"]:checked').val();
+  }
+  
+  function displayNext() {
+    quiz.fadeOut(function() {
+      $('#question').remove();
+      
+      if(counter < questions.length){
+        var nextQuestion = createQuestion(counter);
+        quiz.append(nextQuestion).fadeIn();
+        if (!(isNaN(selections[counter]))) {
+          $('input[value='+selections[counter]+']').prop('checked', true);
+        }
+   if(counter === 0){
+      $('#next').show();
+        }
+      }else {
+        var scoreElem = displayScore();
+        quiz.append(scoreElem).fadeIn();
+        $('#next').hide();
+      }
+    });
+  }
+  
+  function displayScore() {
+    var score = $('<p>',{id: 'question'});
+    
+    var numCorrect = 0;
+    for (var i = 0; i < selections.length; i++) {
+      if (selections[i] === questions[i].answer) {
+        numCorrect++;
+      }
+    }
+    
+    score.append('You got ' + numCorrect + ' questions out of ' +
+                 questions.length + ' right');
+    return score;
+  }
+})();
