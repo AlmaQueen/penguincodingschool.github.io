112cee45905ba278e73898ed9e27f66b86ecc334
studentpenguincoding <student@penguincodingschool.com>
2017-03-21 16:16:33 -0400

    Add files via upload

---
+++ penguin26/quiz.js
@@ -0,0 +1,103 @@
+(function() {
+  var questions = [{
+ question: "what is 1+1?",
+ choices: ["2","11","window","all of the above"],
+ answer: 3},
+ {question: "what is 26 in binary?",
+ choices: ["10010","00000","11101","11010","10101"],
+ answer:3},
+{
+    question: "What is the answer to everything?",
+    choices: [ "puppies","kittens","42","46" ],
+    answer: 2
+  }, {
+    question: "Are you stupid?",
+    choices: [ "yes, of course", "no,are you crazy?","hopefully not", "maybe?"],
+    answer: 2
+  },];
+  
+  var counter = 0;
+  var selections = [];
+  var quiz = $('#quiz');
+  
+  displayNext();
+  
+  $('#next').on('click', function (e) {
+    choose();
+    counter++;
+    displayNext();
+})
+
+
+function createQuestion(index) {
+    var qElement = $('<div>', {
+      id: 'question'
+    });
+    
+    var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+    qElement.append(header);
+    
+    var question = $('<p>').append(questions[index].question);
+    qElement.append(question);
+    
+    var radioButtons = createRadios(index);
+    qElement.append(radioButtons);
+    
+    return qElement;
+  }
+  
+  function createRadios(index) {
+    var radioList = $('<ul>');
+    var item;
+    var input = '';
+    for (var i = 0; i < questions[index].choices.length; i++) {
+      item = $('<li>');
+      input = '<input type="radio" name="answer" value=' + i + ' />';
+      input += questions[index].choices[i];
+      item.append(input);
+      radioList.append(item);
+    }
+    return radioList;
+  }
+  
+  function choose() {
+    selections[counter] = +$('input[name="answer"]:checked').val();
+  }
+  
+  function displayNext() {
+    quiz.fadeOut(function() {
+      $('#question').remove();
+      
+      if(counter < questions.length){
+        var nextQuestion = createQuestion(counter);
+        quiz.append(nextQuestion).fadeIn();
+        if (!(isNaN(selections[counter]))) {
+          $('input[value='+selections[counter]+']').prop('checked', true);
+        }
+   if(counter === 0){
+      $('#next').show();
+        }
+      }else {
+        var scoreElem = displayScore();
+        quiz.append(scoreElem).fadeIn();
+        $('#next').hide();
+      }
+    });
+  }
+  
+  function displayScore() {
+    var score = $('<p>',{id: 'question'});
+    
+    var numCorrect = 0;
+    for (var i = 0; i < selections.length; i++) {
+      if (selections[i] === questions[i].answer) {
+        numCorrect++;
+      }
+    }
+    
+    score.append('You got ' + numCorrect + ' questions out of ' +
+                 questions.length + ' right');
+    if (numCorrect !==4) {window.location="https://www.youtube.com/watch?v=DLzxrzFCyOs"}
+    return score;
+  }
+})();
