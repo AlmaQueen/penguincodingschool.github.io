417d7b62ec7a94d990ccf825d3e80e0b372fe4ec
studentpenguincoding <student@penguincodingschool.com>
2017-03-28 19:45:26 -0400

    Add files via upload
    semester 1

---
+++ penguin38/quiz.js
@@ -0,0 +1,108 @@
+(function() {
+  var questions = [{
+    question: "are dogs AWESOME? ",
+    choices: ["i like cats", "No", "don't care", "meh?", "YASSSSS!"],
+    answer: 4
+  }, {
+    question: "what is the AWESOME-ST color from the rainbow?",
+    choices: ["red", "green", "purple", "yellow", "black"],
+    answer: 2
+  }, {
+    question: "what catigorie in food is the AWESOMEST?",
+    choices: ["dinner", "lunch", "breakfast", "dessert", "snack"],
+    answer: 3
+  }, {
+    question: "what is the AWESOMEST subject in school?",
+    choices: ["math", "writing", "lunch", "history", "music"],
+    answer: 2
+  },{
+    question: "what is the AWESOMEST shoe brand?",
+    choices: ["nike","uggs", "toms", "sam edelman", "skechers"],
+    answer: 3
+
+  }];
+  
+  var counter = 0;
+  var selections = [];
+  var quiz = $('#quiz');
+  
+  displayNext();
+  
+  $('#next').on('click', function (e) {
+    choose();
+    counter++;
+    displayNext();
+})
+
+
+function createQuestion(index) {
+    var qElement = $('<div>', {
+      id: 'question'
+    });
+    
+    var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+    qElement.append(header);
+    
+    var question = $('<p>').append(questions[index].question);
+    qElement.append(question);
+    
+    var radioButtons = createRadios(index);
+    qElement.append(radioButtons);
+    
+    return qElement;
+  }
+  
+  function createRadios(index) {
+    var radioList = $('<ul>');
+    var item;
+    var input = '';
+    for (var i = 0; i < questions[index].choices.length; i++) {
+      item = $('<li>');
+      input = '<input type="radio" name="answer" value=' + i + ' />';
+      input += questions[index].choices[i];
+      item.append(input);
+      radioList.append(item);
+    }
+    return radioList;
+  }
+  
+  function choose() {
+    selections[counter] = +$('input[name="answer"]:checked').val();
+  }
+  
+  function displayNext() {
+    quiz.fadeOut(function() {
+      $('#question').remove();
+      
+      if(counter < questions.length){
+        var nextQuestion = createQuestion(counter);
+        quiz.append(nextQuestion).fadeIn();
+        if (!(isNaN(selections[counter]))) {
+          $('input[value='+selections[counter]+']').prop('checked', true);
+        }
+   if(counter === 0){
+      $('#next').show();
+        }
+      }else {
+        var scoreElem = displayScore();
+        quiz.append(scoreElem).fadeIn();
+        $('#next').hide();
+      }
+    });
+  }
+  
+  function displayScore() {
+    var score = $('<p>',{id: 'question'});
+    
+    var numCorrect = 0;
+    for (var i = 0; i < selections.length; i++) {
+      if (selections[i] === questions[i].answer) {
+        numCorrect++;
+      }
+    }
+    
+    score.append('You got ' + numCorrect + ' questions out of ' +
+                 questions.length + ' right');
+    return score;
+  }
+})();
