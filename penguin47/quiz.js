121f9d94a64db4c8e11bc9dc1c8e80e43f3cf0ad
studentpenguincoding <student@penguincodingschool.com>
2017-03-28 19:27:37 -0400

    Add files via upload

---
+++ penguin47/quiz.js
@@ -0,0 +1,103 @@
+(function() {
+  var questions = [{
+    question: "Where will the Winter 2018 Olympics be?",
+    choices: ["Canada","Pyongcheng","Pyongyang","Boston" ],
+    answer:1
+  }, {
+    question: "Which city held both summer and winter olympics?",
+    choices: ["Beijing","London","Athens" , "Los Angeles" ],
+    answer:0
+  }, {
+    question: "How many contries are there in the world?",
+    choices: ["300","176","196","203"],
+    answer: 2
+  }, {
+    question: "What tropical place deside to bid for? he winter olympics",
+    choices:[ "Dubai" ,"Quatar" ,"Russia" ,"USA" , "Peru"],
+    answer:1
+  }];
+  
+  var counter = 0;
+  var selections = [];
+  var quiz = $('#quiz');
+  
+  displayNext();
+  
+  $('#next').on('click', function (e) {
+    choose();
+    counter++;
+    displayNext();
+})
+
+
+function createQuestion(index) {
+    var qElement = $('<div>', {
+      id: 'question'
+    });
+    
+    var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+    qElement.append(header);
+    
+    var question = $('<p>').append(questions[index].question);
+    qElement.append(question);
+    
+    var radioButtons = createRadios(index);
+    qElement.append(radioButtons);
+    
+    return qElement;
+  }
+  
+  function createRadios(index) {
+    var radioList = $('<ul>');
+    var item;
+    var input = '';
+    for (var i = 0; i < questions[index].choices.length; i++) {
+      item = $('<li>');
+      input = '<input type="radio" name="answer" value=' + i + ' />';
+      input += questions[index].choices[i];
+      item.append(input);
+      radioList.append(item);
+    }
+    return radioList;
+  }
+  
+  function choose() {
+    selections[counter] = +$('input[name="answer"]:checked').val();
+  }
+  
+  function displayNext() {
+    quiz.fadeOut(function() {
+      $('#question').remove();
+      
+      if(counter < questions.length){
+        var nextQuestion = createQuestion(counter);
+        quiz.append(nextQuestion).fadeIn();
+        if (!(isNaN(selections[counter]))) {
+          $('input[value='+selections[counter]+']').prop('checked', true);
+        }
+   if(counter === 0){
+      $('#next').show();
+        }
+      }else {
+        var scoreElem = displayScore();
+        quiz.append(scoreElem).fadeIn();
+        $('#next').hide();
+      }
+    });
+  }
+  
+  function displayScore() {
+    var score = $('<p>',{id: 'question'});
+    
+    var numCorrect = 0;
+    for (var i = 0; i < selections.length; i++) {
+      if (selections[i] === questions[i].answer) {
+        numCorrect++;
+      }
+    }
+    
+    score.append('You got ' + numCorrect + ' questions out of ' +
+                 questions.length + ' right');
+    return score;
+  }
+})();
