9d5793deb13edc82414a50e68fb9b2fb7d09629b
studentpenguincoding <student@penguincodingschool.com>
2017-04-10 18:58:52 -0400

    Add files via upload

--- penguin72/game.js
+++ penguin72/game.js
@@ -8,12 +8,48 @@ img_player.src="http://images.vectorhq.com/images/thumbs/f92/hedgehog-vector-gra
 
 var x =1000;
 var y=1000;
+var xspeed = 10;
+var yspeed = 10;
 
 function animate() {
   requestAnimationFrame(animate);
   cx.clearRect(0,0,canvas.width, canvas.height);
   cx.drawImage(img_player,x,y);
-  x-=1; y-=1
+  x+=xspeed;
+  y+=yspeed;
+  if (x>canvas.width||x<0) {xspeed=-xspeed}
+  if (y>canvas.height ||y<0) {yspeed=-yspeed}
 }
 
 animate();
+
+function setDirection(dir) {
+  if(dir==="up") {
+  xspeed =0;
+  yspeed = -5;
+ } else if(dir==="down") {
+  xspeed =0;
+  yspeed =5;
+ } else if(dir==="left") {
+  xspeed = -5;
+  yspeed =0;
+ } else if(dir==="right") {
+  xspeed =5;
+  yspeed =0;
+ } else if(dir==="stop") {
+  xspeed =0;
+  yspeed =0;
+ }
+}
+var keyActions = {
+  32: "stop",
+  37: "left",
+  38: "up",
+  39: "right",
+  40: "down"
+};
+
+document.addEventListener ('keydown',function(event) {
+  var dir =keyActions[event.keyCode];
+  setDirection(dir);
+});
