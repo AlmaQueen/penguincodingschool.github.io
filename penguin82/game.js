1548fcdbc10f7d4a9714b232cf1cd057e9807ea9
studentpenguincoding <student@penguincodingschool.com>
2017-04-25 19:55:00 -0400

    Add files via upload
    fixed issue with penguin not moving based on commands.Error was due to capitalization issues with the two variables.

--- penguin82/game.js
+++ penguin82/game.js
@@ -1,4 +1,4 @@
-
+'use strict';
 var canvas = document.getElementById("canvas");
 var cx = canvas.getContext("2d");
 canvas.width = 1000;
@@ -9,17 +9,18 @@ img_player.src = "https://static.giantbomb.com/uploads/original/0/3683/1120634-p
 
 var x = 0;
 var y = 0;
-var xspeed = 1;
-var yspeed = 1;
+var xSpeed = 1;
+var ySpeed = 1;
+var gravity = 1;
 
 function animate() {
   requestAnimationFrame(animate);
   cx.clearRect(0,0,canvas.width, canvas.height);
   cx.drawImage(img_player, x, y,50,50);
-  x+=xspeed;
-  y+=yspeed;
-  if (x>canvas.width||x<0) {xspeed=-xspeed}
-  if (y>canvas.height || y<0) {yspeed = -yspeed}
+  x+=xSpeed;
+  y+=ySpeed + gravity;
+  if (x>canvas.width||x<0) {xSpeed=-xSpeed}
+  if (y>canvas.height || y<0) {ySpeed = -ySpeed}
   }
 
 function setDirection(dir) {
@@ -50,74 +51,11 @@ var keyActions = {
 } ;
 
 document.addEventListener('keydown',function(event) {
-var dir = keyActions[event.keyCode] ;
-setDirection("dir") ;
+var dir = keyActions[event.keyCode];
+setDirection(dir) ;
 });
 document.addEventListener('keyup',function(event) {
-var dir = keyActions[event.keyCode] ;
+var dir = keyActions[event.keyCode];
 setDirection("stop") ;
 });
 animate();
-
-
-// var canvas = document.getElementById("canvas");
-// var cx = canvas.getContext("2d");
-// canvas.width=1000;
-// canvas.height=1000;
-
-// var img_player = document.createElement("img");
-// img_player.src = "https://static.giantbomb.com/uploads/original/0/3683/1120634-penguin_chick.jpg";
-// var x=0;
-// var y=0;
-// var xSpeed =0;
-// var ySpeed =0;
-// var gravity =1;
-
-// function animate () {
-//   requestAnimationFrame(animate);
-//   cx.clearRect (0,0,canvas.width, canvas.height);
-//   cx.drawImage(img_player, x, y,100,100);
-//   x+=xSpeed;
-//   y+=ySpeed+gravity;
-// if (x>canvas.width-250 ||x<0) {xSpeed=-xSpeed}
-// if (y>canvas.height-250 || y<0) {ySpeed = -ySpeed}
-  
-// }
-// function setDirection(dir) {
-//   if (dir=="up") {
-//     xSpeed = 0;
-//     ySpeed = -5;
-//   }  else if (dir=="down") {
-//     xSpeed = 0;
-//     ySpeed = 5;
-//   }  else if (dir=="left") {
-//     xSpeed = -5;
-//     ySpeed = 0;
-//   }  else if (dir=="right") {
-//     xSpeed = 5;
-//     ySpeed = 0;
-//   }  else if (dir=="stop") {
-//     xSpeed = 0;
-//     ySpeed = 0;
-//   }
-// }
-
-// var keyActions = {
-//   32: "stop",
-//   37: "left",
-//   38: "up",
-//   39: "right",
-//   40: "down"
-// };
-
-// document.addEventListener('keydown', function(event) {
-//   var dir = keyActions[event.keyCode];
-// setDirection("dir");
-// });
-
-// document.addEventListener('keyup', function(event) {
-//   var dir = keyActions[event.keyCode];
-//   setDirection("stop");
-// });
-
-// animate()
