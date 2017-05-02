c2833c2e338404df498debd0ec7e80ee7d8999b0
studentpenguincoding <student@penguincodingschool.com>
2017-05-02 19:40:44 -0400

    Add files via upload

---
+++ penguin77/script.js
@@ -0,0 +1,11 @@
+function talk() {
+  var el = document.getElementById("textbox");
+  var x = document.getElementById("reply");
+  if (x.selectedIndex ==1) {
+    el.textContent = "My name is Olivia"
+  } else if (x.selectedIndex ==2) {
+    el.textContent = "My name is Olivia, what is your name?";
+  } else if (x.selectedIndex ==3) {
+    el.textContent = "Of course! Where is your nest?";
+  } else {el.textContent = "I'm not sure what you mean."}
+}
