c280ab1bc19ce20e7bf0003c5fc169d9b9f158d3
studentpenguincoding <student@penguincodingschool.com>
2017-05-06 15:00:01 -0400

    Add files via upload

---
+++ penguin14/story/story.js
@@ -0,0 +1,7 @@
+function newtext() {
+  var el = document.getElementById("textbox");
+  var el1 = document.getElementById("textbox1");
+  el.textContent ="Yay! Do you want to go to school with me? I promise it will be fun!";
+  el1.textContent = "";
+  
+}
