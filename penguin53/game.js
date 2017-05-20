16505b72b98d614380823d4b83b7c15cb78d3867
studentpenguincoding <student@penguincodingschool.com>
2017-05-20 11:59:33 -0400

    Add files via upload

--- penguin53/game.js
+++ penguin53/game.js
@@ -17,15 +17,17 @@ var playerW = 50;
 var plat=[]
 var req;
 
-plat.push({x:0, y:200, w:100, h:10})
+plat.push({x:175, y:100, w:100, h:10})
 plat.push({x:250, y:200, w:100, h:10})
-plat.push({x:500, y:300, w:100, h:10})
-plat.push({x:250, y:400, w:100, h:10})
-plat.push({x:0, y:900, w:990, h:10})
+plat.push({x:350, y:300, w:100, h:10})
+plat.push({x:500, y:400, w:100, h:10})
+plat.push({x:0, y:900, w:999, h:10})
 plat.push({x:700, y:700, w:100, h:10})
 plat.push({x:300, y:800, w:105, h:10})
 plat.push({x:500, y:750, w:105, h:10})
 plat.push({x:500, y:650, w:105, h:10})
+plat.push({x:700, y:600, w:100, h:10})
+plat.push({x:600, y:500, w:100, h:10})
 
 function animate() {
   req = requestAnimationFrame(animate);
@@ -35,6 +37,7 @@ function animate() {
    y+=ySpeed+gravity;
    platform();
    obstacle();
+   monster();
    if (x>canvas.width||x<0) {xSpeed=-xSpeed}
    if (y>canvas.height || y<0) {ySpeed = -ySpeed}
    if (y<0) {gameWin();}
@@ -106,9 +109,9 @@ function platform() {
 }
 
 var lava = [];
-lava.push ({x:0, y:300, w:100, h:10})
-lava.push ({x:300, y:100, w:150, h:10})
-lava.push ({x:250, y:500, w:250, h:10})
+lava.push ({x:0, y:300, w:100, h:10});
+lava.push ({x:300, y:100, w:150, h:10});
+lava.push ({x:250, y:500, w:250, h:10});
 
 function obstacle() {
   cx.fillStyle="red";
@@ -118,6 +121,28 @@ function obstacle() {
     x>=lava[i].x-playerW &&
     x<=lava[i].x + lava[i].w)
     {gameOver()}
+  }
+}
+
+var xM = 800;
+var yM = 650;
+var yMsp = 0;
+var xMsp = -4;
+
+var mW =30;
+var mH =30;
+
+var img_monster = document.createElement("img");
+img_monster.src = "https://vignette2.wikia.nocookie.net/fantendo/images/5/54/BooNSMBWii.png/revision/latest?cb=20120922005024"
+
+function monster() {
+  cx.drawImage(img_monster, xM,yM, mW, mH);
+  xM += xMsp;
+  yM += yMsp;
+  if (x+playerW > xM && xM+mW >x && yM+mH > y && y+playerH > yM) {
+    gameOver ();
+  } else if (xM<275 || xM>canvas.width) {
+    xMsp = -xMsp;
   }
 }
 
