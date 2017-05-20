8891f9734b7efe0ceb2f8db42da0c9775db15b21
studentpenguincoding <student@penguincodingschool.com>
2017-05-20 09:59:48 -0400

    Add files via upload

--- penguin2/story/story.js
+++ penguin2/story/story.js
@@ -9,6 +9,8 @@ function talk() {
     el.textContent = "this is a very safe place"
   } else if  (x.selectedIndex ==4) {
     el.textContent = "Thorn the evil dragon is taking over the dragon kingdom, and this is the only safe place to fly over";
+  } else if  (x.selectedIndex ==5) {
+    el.textContent = "i will tell you my name if you tell me yours first";
     purpledragon();
   
   } else {}
@@ -17,9 +19,29 @@ function talk() {
 }
 
 function purpledragon() {
-  
   var ele = document.getElementById("purpledragon1") ;
-  if(ele.style.display =="none") {
-    ele.style.display =="block";
+  if (ele.style.display ==="none") {
+    ele.style.display ="block";
+  } else {}
+}
+
+function purpledragon2() {
+  var ele = document.getElementById("purpledragon2") ;
+  if (ele.style.display ==="none") {
+    ele.style.display ="block";
+  } else {}
+}
+
+function purpledragonform() {
+   var el = document.getElementById("textbox");
+   var ele = document.getElementById("purpledragon1");
+   var ele2 = document.getElementById("purpledragon2");
+   var name = prompt("My name is: ");
+   el.textContent = "Hello "+name + ". My name is glinia. Do you want to come to my cave? it is not safe to talk here.";
+   purpledragon2();
+   ele.style.display = "none";
+     
+  if(ele2.style.display =="none") {
+    ele2.style.display ="block";
   } else {}
 }
