3076c6a243dfe62540f6bc081a5eefeb2025f4bd
studentpenguincoding <student@penguincodingschool.com>
2017-05-20 10:01:19 -0400

    Add files via upload

---
+++ penguin1/story/story.js
@@ -0,0 +1,25 @@
+function talk() {
+  var el=document.getElementById("textbox");
+  var x=document.getElementById("reply");
+  if (x.selectedIndex==1){
+    el.textContent="Maybe.";
+  }else if (x.selectedIndex==2){
+  el.textContent="My name is Mr.Sean the Seagull. You can call me Sean.";
+}else if (x.selectedIndex==3){
+  el.textContent="I think so. Are you the lost moose Ellie?";
+  family();
+  }else if ()
+  else  {el.textContent="You are weird. What do you mean?"}
+}
+function family() {
+  var ele = document.getElementById("familyform");
+if(ele.style.display=="none") {
+ele.style.display ="block"
+}else{}
+}
+
+function family1(){
+  
+
+  
+}
