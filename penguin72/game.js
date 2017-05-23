00a9f5074f9597617a7b56b2a41f352854b1476c
studentpenguincoding <student@penguincodingschool.com>
2017-05-23 19:32:01 -0400

    Add files via upload

--- penguin72/game.js
+++ penguin72/game.js
@@ -1,10 +1,13 @@
+'use strict';
+
+
 var canvas = document.getElementById("canvas");
 var cx = canvas.getContext("2d");
 canvas.width=1597;
 canvas.height=1101;
 
 var img_player= document.createElement("img");
-img_player.src="http://shmector.com/_ph/8/228098179.png"
+img_player.src="http://shmector.com/_ph/8/228098179.png";
 
 var x =0;
 var y=0;
@@ -15,23 +18,19 @@ var playerW = 30;
 var playerH = 35;
 var req;
 function animate() {
-  requestAnimationFrame(animate);
+  req = requestAnimationFrame(animate);
   cx.clearRect(0,0,canvas.width, canvas.height);
   cx.drawImage(img_player,x,y,playerW, playerH);
   x+=xspeed;
   y+=yspeed + gravity;
   platform();
   obstacle();
+  monster();
+  coin();
+  scoreDisplay();
   if (x>canvas.width||x<0) {xspeed=-xspeed}
   if (y>canvas.height ||y<0) {yspeed=-yspeed}
   }
-function stop() {
-  if(req === true){
-    cancelAnimationFrame(req);
-    req = undefined;
-    
-  }
-}
 
 function setDirection(dir) {
   if(dir==="up") {
@@ -104,7 +103,7 @@ if (y==plat[i].y-playerH &&
 
 function obstacle() {
 cx.fillStyle= "red";
-for (var i = 0; i<plat.length; i++) {
+for (var i = 0; i<lava.length; i++) {
  cx.fillRect(lava[i].x, lava[i].y, lava[i].w, lava[i].h);
 if (y==lava[i].y-playerH &&
    x>=lava[i].x-playerW &&
@@ -113,5 +112,59 @@ if (y==lava[i].y-playerH &&
 }
 }
 
+var xM = 800;
+var yM = 80;
+var xMsp = -5;
+var yMsp = 0;
+
+var mW = 10;
+var mH = 10;
+
+function monster(){
+  cx.fillStyle = "orange";
+  cx.fillRect(xM,yM,mW,mH);
+  xM+=xMsp;
+  if (x+playerW > xM && xM+mW >x && yM+mH>y && y+playerH>yM)
+  {gameOver()}
+  else if (xM<0 || xM>canvas.width) {
+    xMsp = -xMsp;
+  }
+}
+var xC = 800;
+var yC = 80;
+
+var cW = 10;
+var cH = 10;
+var score =0;
+
+function coin(){
+  cx.fillStyle = "green";
+  cx.fillRect(xC,yC,cW,cH);
+  if (x+playerW > xC && xC+cW >x && yC+cH>y && y+playerH>yC)
+  {
+  score+=10;
+  xC = Math.random()*canvas.width;
+  yC = Math.random()*canvas.height;
+  }
+}
+
+function scoreDisplay() {
+   cx.fillStyle = "Green";
+   cx.font = "30px Comic Sans MS";
+   cx.fillText("Score "+score,10,50);
+ }
+
+ function gameOver() {
+   cx.fillStyle = "Red";
+   cx.font = "30px Comic Sans MS";
+   cx.fillText("game Over",10,50);
+   stop();
+ }
+ 
+ function stop() {
+   if(req){
+     cancelAnimationFrame(req);
+     req = undefined;
+}}
 
 animate();
