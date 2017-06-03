dec74460b8b091cdc3f1af272c33f5b8f322dd23
studentpenguincoding <student@penguincodingschool.com>
2017-06-03 09:52:46 -0400

    Add files via upload

--- penguin1/story/story.js
+++ penguin1/story/story.js
@@ -4,13 +4,14 @@ function talk() {
   if (x.selectedIndex==1){
     el.textContent="Maybe.";
   }else if (x.selectedIndex==2){
-  el.textContent="My name is Mr.Sean the Seagull. You can call me Sean.";
-}else if (x.selectedIndex==3){
+  el.textContent="My name is Mr. Sean the Seagull. You can call me Sean.";
+  }else if (x.selectedIndex==3){
   el.textContent="I think so. Are you the lost moose Ellie?";
   family();
-  }else if ()
+}
   else  {el.textContent="You are weird. What do you mean?"}
 }
+
 function family() {
   var ele = document.getElementById("familyform");
 if(ele.style.display=="none") {
