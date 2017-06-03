6e2038cbd453db57cf0d1bc9d5a14ead971e455c
studentpenguincoding <student@penguincodingschool.com>
2017-06-03 14:56:00 -0400

    Add files via upload

--- penguin3/story/script.js
+++ penguin3/story/script.js
@@ -3,10 +3,10 @@ function talk() {
   var x=document.getElementById('conversation');
   var el=document.getElementById('reply');
   if(x.selectedIndex==1){
-el.textContent = "hello to you to"}
+el.textContent = "Hola amigo"}
  else if(x.selectedIndex==2){
  el.textContent="i'm a famous puppy surfer"}
  else if(x.selectedIndex==3){
- el.textContent="it's over there somewhere."}
+ el.innerHTML="<a href=wherever.html>follow me.</a>"}
   
 }
