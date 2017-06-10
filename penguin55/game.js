75be578c94ca8b9ff6eb3b7aecf2854cd4fd2d6f
studentpenguincoding <student@penguincodingschool.com>
2017-06-10 12:05:09 -0400

    Add files via upload

--- penguin55/game.js
+++ penguin55/game.js
@@ -6,8 +6,7 @@ canvas.height=850;
 
 var img_player = document.createElement("img");
 img_player.src = "https://s-media-cache-ak0.pinimg.com/originals/b8/03/be/b803becf10c28afad61b6f3e5a394d5c.jpg"
-var mp = document.getElement("img");
-mp.src = "https://s-media-cache-ak0.pinimg.com/originals/0a/1f/89/0a1f897921a2e94cb3621199f2adef19.jpg"
+
 var x = 0;
 var y = 440;
 
@@ -21,13 +20,21 @@ var playerW = 50;
 var plat =[];
 var lava=[];
 var lavaImg = document.createElement("img");
-var xM = 500;
-var yM = 0;
+var xM = 1000;
+var yM = 300;
 var xMS = -5;
 var yMS = 0;
 var mW = 50;
 var mH = 50;
-lavaImg.src = "http://www.pngmart.com/files/2/Pac-Man-Ghost-PNG-Transparent-Image.png"
+var xC = 1000;
+var yC = 330;
+var cH = 20;
+var cW = 20;
+var score = 0;
+var mp = document.createElement("img");
+lavaImg.src = "lava.jpg";
+
+mp.src = "http://www.pngmart.com/files/2/Pac-Man-Ghost-PNG-Transparent-Image.png";
 
 plat.push({x :0, y: 100, w:100, h:10});
 plat.push({x :125, y: 125, w:100, h:10});
@@ -52,10 +59,14 @@ lava.push({x :0, y: 650, w:canvas.width, h:300});
 function stop(){
   if (req){
     cancelAnimationFrame(req);
-    req = undefined
+    req = undefined;
   }
 }
-
+function scoreDisplay(){
+  cx.fillStyle = "green";
+  cx.font = "30px Comic Sans MS";
+  cx.fillText("+score,850,100")
+}
 function gameOver(){
   cx.fillStyle = "red";
   cx.font = "50px Comic Sans MS";
@@ -71,18 +82,47 @@ function gameWin(){
 function animate(){
   req = requestAnimationFrame(animate);
   cx.clearRect(0,0,canvas.width, canvas.height);
-  cx.drawImage(img_player, x,y,playerW, playerW);
+  cx.drawImage(img_player, x,y,playerW, playerH);
   x+=xspeed;y+=yspeed-gravity;
   platform();
   obstacle();
+  monster();
+  coin();
   if (x>canvas.width || x<0){xspeed=-xspeed}
     if (y>canvas.height || y<0){yspeed=-yspeed}
     if(x<100 && y< 100){
       gameWin();
     }
 }
+function coin() {
+  cx.fillStyle = "gold";
+  cx.fillRect(xC,yC, cW, cH);
+  if (x+playerW > xC && xC+cW>x && yM+cH > y &&y+playerH > yC) {
+    score+=10;
+    var i = getRandomInt(1,plat.length);
+    xC = plat[i].x;
+    yC = plat[i].y-20;
+    //xC = Math.random()*canvas.width-100);
+    //yCgetRandomInt(1,3)*150;
+}
+function getRandomInt(min,max){
+  min = Math.ceil(min);
+  max = Math.ceil(max);
+  return Math.floor(Math.random()*(max-min))+min;
+}
+}
 function monster() {
-  
+  cx.drawImage(mp, xM,yM,mW, mH);
+  xM += xMS;
+  yM+= yMS;
+  if (x+playerW > xM && xM+mW>x && yM+mH > y &&y+playerH > yM) {
+    gameOver();
+  }
+  else if (xM<1000){
+    xMS = -xMS;
+  }else if (xM>1400){
+    xMS= -xMS;
+  }
 }
 
 
@@ -129,7 +169,7 @@ document.addEventListener("keyup", function(event) {
 function platform() {
   gravity =-5;
   cx.fillStyle="blue";
-for (var i = 0; i<plat.length; i++) {
+  for (var i = 0; i<plat.length; i++) {
   cx.fillRect(plat[i].x,plat[i].y,plat[i].w,plat[i].h);
   if (y==plat[i].y-playerH &&
   x>=plat[i].x-playerW &&
