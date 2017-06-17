dd8f2d36cb95884a81a9bf2258b60e851d0ccd8b
studentpenguincoding <student@penguincodingschool.com>
2017-06-17 14:58:14 -0400

    Add files via upload

--- penguin14/story/story.js
+++ penguin14/story/story.js
@@ -1,10 +1,10 @@
 function newtext() {
   var el = document.getElementById("textbox");
-  var el1 = document.getElementById("textbox1");
+  //var el1 = document.getElementById("textbox1");
   var el11= document.getElementById("choices");
   var el2 = document.getElementById("choices2");
   el.textContent ="Cool! Do you want to go to school with me? I promise that it will be fun!";
-  el1.textContent = "";
+  //el1.textContent = "";
   el11.style.display="none";
   if(el2.style.display==="none") {
     el2.style.display="block";
