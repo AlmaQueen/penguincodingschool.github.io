38f99070b0118830b8c9eb90fca7e236edc98906
volunteerpenguin <volunteer@penguincodingschool.com>
2017-07-10 05:10:38 -0400

    cleanup for summer 2017

--- pensharpener/scripts.js
+++
@@ -1,5 +0,0 @@
-function askQuestions() {
-  alert("Hi my name is pensharpener");
-  var name = prompt('What is your name?');
-  confirm("Nice to meet you" + name);
-}
--- puckcruncher/scripts.js
+++
@@ -1,5 +0,0 @@
-function askQuestions() {
-  alert("Hi my name is Sharky");
-  var name=prompt ('What is your name?');
-  confirm ("Nice to meet you"+name);
-}
