ac958a9abb249245d029cbbf4f62fffebf14d7a6
studentpenguincoding <student@penguincodingschool.com>
2017-07-11 14:51:10 -0400

    Add files via upload

--- penguin103/game.js
+++ penguin103/game.js
@@ -8,19 +8,20 @@ img_player.src = "butterfly.png";
 
 var x = 0;
 var y = 0;
-var w = 50;
-var h = 50;
+var playerW = 50;
+var playerH = 50;
 var xspeed =0;
 var yspeed =0;
-var gravity =1;
+var gravity =5;
 
 function animate() {
-  requestAnimationFrame(animate);
+  req = requestAnimationFrame(animate);
   cx.clearRect(0,0,canvas.width,canvas.height);
-  cx.drawImage(img_player,x,y,w,h);
+  cx.drawImage(img_player,x,y,playerW,playerH);
   x+=xspeed;
   y+=yspeed+gravity;
-  
+  platform();
+  obstacle();
 if (x<0){
    xspeed = -xspeed;
 }
@@ -56,9 +57,13 @@ function setDirection(dir) {
   xspeed= 0;
   yspeed= 0;
   }
+   if(dir =="jump") {
+  xspeed= 0;
+  yspeed= -10;
+  }
 }
 var keyActions = {
-  32: "stop",
+  32: "jump",
   37: "left",
   38: "up",
   39: "right",
@@ -73,6 +78,86 @@ document.addEventListener('keydown',function(event){
 document.addEventListener('keyup',function(event){
   var dir = keyActions[event.keyCode];
   setDirection("stop");
-});
+})
 
+var plat =[];
+plat.push({x:0, y:300, w:100, h:10});
+plat.push({x:150, y:200, w:100, h:10});
+plat.push({x:250, y:300, w:100, h:10});
+plat.push({x:350, y:350, w:100, h:10});
+plat.push({x:550, y:450, w:100, h:10});
+plat.push({x:750, y:300, w:100, h:10});
+plat.push({x:850, y:350, w:100, h:10});
+plat.push({x:550, y:150, w:100, h:10});
+
+function platform() {
+  gravity=5;
+  cx.fillStyle="pink";
+  for (var i = 0; i<plat.length; i++)
+  {cx.fillRect(plat[i].x, plat[i].y, plat[i].w,
+    plat[i].h);
+  if(y==plat[i].y-playerH &&
+  x>=plat[i].x-playerW &&
+  x<=plat[i].x+plat[i].w)
+  {gravity=0}
+  else{}
+  }
+}
+function platform() {
+  gravity=5;
+  cx.fillStyle="pink";
+  for (var i = 0; i<plat.length; i++)
+  {cx.fillRect(plat[i].x, plat[i].y, plat[i].w,
+    plat[i].h);
+  if(y==plat[i].y-playerH &&
+  x>=plat[i].x-playerW &&
+  x<=plat[i].x+plat[i].w)
+  {gravity=0}
+  else{}
+  }
+}
+
+function platform() {
+  gravity = 5 ;
+  cx.fillStyle="pink";
+  for (var i = 0; i<plat.length; i++)
+  {cx.fillRect(plat[i].x, plat[i].y, plat[i].w,
+    plat[i].h);
+  if(y==plat[i].y-playerH &&
+  x>=plat[i].x-playerW &&
+  x<=plat[i].x+plat[i].w)
+  {gravity=0}
+  else{}
+  }
+}
+
+var lava =[];
+lava.push({x:0,y:650,w:1000,h:50})
+
+function obstacle() {
+  cx.fillStyle="orange";
+  for (var i = 0; i<lava.length; i++)
+  {cx.fillRect(lava[i].x, lava[i].y, lava[i].w,
+    plat[i].h);
+  if(y==lava[i].y-playerH &&
+  x>=lava[i].x-playerW &&
+  x<=lava[i].x+lava[i].w)
+  {gameover()}
+  else{}
+  }
+}
+
+function gameover() {
+  cx.fillStyle = "orange";
+  cx.font = "30px Comic Sans MS";
+  cx.fillText("Game Over",10,50);
+  stop();
+}
+
+function stop() {
+  if(req) {
+    cancelAnimationFrame(req);
+    req = undefined;
+  }
+}
 animate();
