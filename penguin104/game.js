0593b5787b96166fae7e3a22fd40fe90b9c811be
studentpenguincoding <student@penguincodingschool.com>
2017-07-12 14:54:03 -0400

    Add files via upload

--- penguin104/game.js
+++ penguin104/game.js
@@ -4,15 +4,15 @@ canvas.width = 1000;
 canvas.height = 700;
 
 var img_player = document.createElement("img");
-img_player.src = "https://upload.wikimedia.org/wikipedia/en/9/99/MarioSMBW.png"
+img_player.src = "bowser1.png"
+//img_player.src = "https://upload.wikimedia.org/wikipedia/en/9/99/MarioSMBW.png"
 var img_player2 = document.createElement("img");
-img_player2.src = "https://upload.wikimedia.org/wikipedia/en/f/f1/LuigiNSMBW.png"
-//var img_player = document.createElement("img");
-//img_player.src = "http://vignette3.wikia.nocookie.net/fantendoimages/c/cd/Goomba_Looking.png/revision/latest?cb=20120731143323"
-//var img_player = document.createElement("img");
-//img_player.src = "http://vignette1.wikia.nocookie.net/fantendo/images/7/7a/NSMBULuigi.png/revision/latest?cb=20121226224332"
-var x = 0; var x2=500;
-var y = 0; var y2=100;
+img_player2.src = "bowser2.png";
+//img_player2.src = "https://upload.wikimedia.org/wikipedia/en/f/f1/LuigiNSMBW.png"
+var x = 0;
+var x2=500;
+var y = 0;
+var y2=100;
 var playerW = 70
 var playerH = 100
 var xspeed = 0; //Mario
@@ -32,16 +32,20 @@ req = requestAnimationFrame(animate);
   x+=xspeed;
   y+=yspeed+gravity;
   x2+=xspeed2;
-  y2+=yspeed2+gravity;
+  y2+=yspeed2+gravity2;
 //  platformG();
   platform();
  // platform2();
   obstacle();
 //  obstacle2();
-  lifeleft()
+  lifeleft();
 //  lifeleft2()
-  
+  monster();
   //Mario
+  coin();
+  
+  scoreDisplay();
+  
   if (x<=0) {
     xspeed = -xspeed;
   }
@@ -128,7 +132,8 @@ document.addEventListener('keydown', function(event) {
 })
 document.addEventListener('keyup', function(event) {
   var dir = keyActions[event.keyCode];
-  setDirection("stop")
+  setDirection("stop");
+  setDirection("stop2");
 })
 
 var plat = [];
@@ -140,7 +145,7 @@ plat.push({x:560, y:350, w:100, h:10, color:"blue"});
 plat.push({x:700, y:400, w:100, h:10, color:"purple"});
 
 function platform() {
-  gravity=5
+  gravity=5; gravity2=5;
 for (var i = 0; i<plat.length; i++)
   { cx.fillStyle=plat[i].color;
     cx.fillRect(plat[i].x, plat[i].y, plat[i].w, plat[i].h)
@@ -151,7 +156,7 @@ for (var i = 0; i<plat.length; i++)
     if(y2== plat[i].y-playerH &&
       x2>=plat[i].x-playerW &&
       x2<=plat[i].x+plat[i].w)
-    {gravity=0}
+    {gravity2=0}
     
    else{}
   }
@@ -169,22 +174,78 @@ function obstacle() {
   x>=lava[i].x-playerW &&
   x<=lava[i].x+lava[i].w)
   {life-=1;
+  x=0;
+  y=0;
    if(life===0) {gameover()}
   }
   else{}
   }
 }
 
+var xM = 800;
+var yM = 180;
+var xMsp = -5;
+var yMsp = -5
+
+var mW = 100;
+var mH = 100;
+
+var img_monster = document.createElement("img");
+img_monster.src = "bowser1.png";
+var img_monster2 = document.createElement("img");
+img_monster2.src = "bowser2.png";
+
+function monster() {
+  cx.drawImage(img_monster, xM, yM, mW, mH);
+  xM+=xMsp;
+  yM+=yMsp;
+  if (x+playerW > xM && xM+mW >x && yM+mH>y && y +playerH>yM)
+  {gameover()}
+  if (xM<0 || xM>canvas.width-mW)
+  {xMsp = -xMsp;}
+    if (yM<0 || yM>canvas.width-mW) {
+    yMsp = -yMsp;
+  }
+}
+
+
+
 function lifeleft() {
   cx.lifeStyle = "green"
   cx.font = "30px Comic Sans MS";
-  cx.fillText("Lives "+life, 875 , 10);
+  cx.fillText("Lives "+life, 875 , 40);
 }
 function gameover() {
   cx.fillStyle = "red";
   cx.font = "30px Comic Sans MS";
   cx.fillText("Game Over", 25, 50);
   stop();
+}
+
+var xC = 80;
+var yC = 50;
+var wC = 30;
+var hC = 30;
+var score = 0;
+
+var img_coin = document.createElement("img");
+img_coin.src = "coin.png";
+
+function coin() {
+  cx.drawImage(img_coin,xC, yC, wC, hC);
+  if (x+playerW > xC && xC+wC >x && yC+hC>y && y +playerH>yC)
+  
+{   score +=10;
+  var i = Math.ceil(Math.random()*plat.length);
+  setTimeout(xC = plat[i].x+20,1000)
+  yC=plat[i].y-40;
+  }
+}
+
+function scoreDisplay(){
+  cx.fillStyle="green";
+  cx.font = "30px 'Comic Sans MS'";
+  cx.fillText("Score "+score, 500, 100);
 }
 
 function stop() {
