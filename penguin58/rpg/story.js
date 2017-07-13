c4c6512e7503272f8c3035f537244c83d17a443f
studentpenguincoding <student@penguincodingschool.com>
2017-07-13 18:03:53 -0400

    Add files via upload

---
+++ penguin58/rpg/story.js
@@ -0,0 +1,9 @@
+function talk(){
+  var x = document.getElementById("question");
+  if (x.selectedIndex ==1) {
+    el.textContent = "Treasure? Well, if I lead you to the treasure, I'll have to take everything. Give me the treasure map- NOW.";
+  } else if (x.selectedIndex ==2) {
+    el.textcontent = "I'll get some for you.";
+  }
+  }
+}
