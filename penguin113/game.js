27758c74c262dcf55833c3684bbf05f9447682c0
studentpenguincoding <student@penguincodingschool.com>
2017-07-13 17:35:52 -0400

    Add files via upload

--- penguin113/game.js
+++ penguin113/game.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById("canvas");
 var cx = canvas.getContext ("2d");
-canvas.width=1000;
-canvas.height=1000;
+canvas.width=1250;
+canvas.height=700;
 
 var img_player = document.createElement("img");
 img_player.src = "mario.png";
@@ -10,18 +10,63 @@ var x =0;
 var y =0;
 var playerW = 40;
 var playerH = 40;
-var xSpeed = 5
+var xSpeed = 0;
+var ySpeed =0;
 
 function animate()  {
-requestAnimationFrame (animate);
-cx.clearRect (0,0,canvas.width, canvas.height);
+requestAnimationFrame(animate);
+cx.clearRect(0,0,canvas.width, canvas.height);
 cx.drawImage(img_player,x,y,playerW, playerH);
 x+=xSpeed;
 if (x>canvas.width || x <0)  {
 xSpeed = -xSpeed;
-   };
+  }
+//var ySpeed = 5;
+y+=ySpeed;
+if (y>canvas.height || y<0) {
+ySpeed = -ySpeed;
+  }
 
+   }
+
+
+
+function setDirection(dir){
+if (dir === "down") {
+  xSpeed = 0;
+  ySpeed = 5;}
+else if (dir === "up") {
+  xSpeed = 0;
+  ySpeed = -5;}
+else if (dir === "left") {
+  xSpeed = -5;
+  ySpeed = 0;}
+else if (dir === "right") {
+  xSpeed = 5;
+  ySpeed = 0;}
+else if (dir === "stop") {
+  xSpeed = 0;
+  ySpeed = 0;
+  }
 }
 
-animate();
 
+var keyActions = {
+  32: "stop",
+  37: "left",
+  38: "up",
+  39: "right",
+  40: "down"
+};
+  
+document.addEventListener('keydown',function(event) {
+  var dir = keyActions[event.keyCode];
+  setDirection(dir);
+});
+document.addEventListener('keyup',function(event) {
+  var dir = keyActions[event.keyCode];
+  setDirection("stop");
+});
+
+
+animate();
