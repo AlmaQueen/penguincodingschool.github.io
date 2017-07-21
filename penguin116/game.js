03470a13c226294a3afcd074a538a3b46a4ad656
studentpenguincoding <student@penguincodingschool.com>
2017-07-21 13:16:39 -0500

    Add files via upload

--- penguin116/game.js
+++ penguin116/game.js
@@ -20,6 +20,9 @@ cx.drawImage(img_player,x,y, playerW,playerH);
 x+=xSpeed;
 y+=ySpeed;
 monster();
+coin();
+treasure();
+scoreDisplay();
 if(x<0 || x>canvas.width){
 xSpeed = -xSpeed;}
 if(y<0 || y>canvas.height){
@@ -38,10 +41,10 @@ if (dir=="down"){
  }
 if (dir=="right"){
    xSpeed =5;
-   ySpeed=-0;
+   ySpeed=0;
  }
 if (dir=="left"){
-   xSpeed =5;
+   xSpeed =-5;
    ySpeed=-0;
    
  }if (dir=="stop"){
@@ -87,7 +90,7 @@ function monster() {
   
 function gameOver() {
   cx.fillStyle = "red";
-  cx.font = 'Comic Sans MS';
+  cx.font = "60px 'Comic Sans MS'";
   cx.fillText("Game Over",250,250);
   stop();
 }
@@ -99,10 +102,70 @@ function stop() {
   }
 }
   
+ 
+var img_coin = document.createElement("img");
+img_coin.src = "butter.jpg";
+ 
+var xC =500;
+var yC =200;
+var wC =80;
+var hC =80;
+var score = 0;
+ 
+function coin() {
+ cx.drawImage(img_coin,xC, yC,wC,hC);
+ if (x+playerW > xC && xC+wC >x && yC+hC>y && y+playerH>yC){
+    score +=10;
+      xC = Math.random()*canvas.width;
+      yC = Math.random()*canvas.height;
+ }
+}
+
+function scoreDisplay(){
+      cx.fillStyle="blue";
+      cx.font ="60px 'Comic Sans MS'";
+      cx.fillText("Score: "+score, 500,100);
+}
+ var gem = [];
+ gem.push({x:100, y:200, w:10,h:10,color:"green"});
+ gem.push({x:290, y:188, w:10,h:10,color:"pink"});
+ gem.push({x:150, y:167, w:10,h:10,color:"red"});
+ gem.push({x:100, y:200, w:10,h:10,color:"yellow"});
+ gem.push({x:20, y:200, w:10,h:10,color:"mint"});
+ gem.push({x:800, y:300, w:10,h:10,color:"orange"});
+ gem.push({x:180, y:200, w:10,h:10,color:"purple"});
+ gem.push({x:900, y:154, w:10,h:10,color:"black"});
+
+      
+function treasure() {
+for (var i = 0; i<gem.length; i++) {
+  cx.fillStyle=gem[i].color;
+  cx.fillRect(gem[i].x, gem[i].y, gem[i].w, gem[i].h);
+  if (x+playerW >gem[i].x &&
+  gem[i].x+gem[i].w >x &&
+  gem[i].y+gem[i].h>y &&
+  y+playerH>gem[i].y)
+  {score+=gem[i].y;
+    gem[i].x=-100; }
+  }
+}
+
 animate();
+      
+   
+  
    
    
    
+   
+   
+   
+ 
+  
+  
+  
+  
+  
    
    
    
