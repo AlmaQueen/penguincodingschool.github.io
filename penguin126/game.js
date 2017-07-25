d0668346670daf60317df752409b488611fc85d4
studentpenguincoding <student@penguincodingschool.com>
2017-07-25 14:45:54 -0400

    Add files via upload

--- penguin126/game.js
+++ penguin126/game.js
@@ -1,15 +1,17 @@
 var canvas = document.getElementById("canvas");
 var cx = canvas.getContext("2d");
-canvas.width = 1000;
+canvas.width = 1300;
 canvas.height = 800;
 
 var img_player = document.createElement("img");
 img_player.src = "16-bitGunvolt.png";
+//var img_platform=document.createElement();
 
-var x =1;
-var y=1;
-var playerW = 45;
-var playerH = 45;
+
+var x =0;
+var y=0;
+var playerW = 90;
+var playerH = 80;
 var xSpeed = 0;
 var ySpeed = 0;
 var gravity = 5;
@@ -18,22 +20,20 @@ var gravity = 5;
 function animate() {
   requestAnimationFrame(animate);
   cx.clearRect(0,0,canvas.width, canvas.height);
-  cx.drawImage(img_player,x,y);
+  cx.drawImage(img_player,x,y,playerW,playerH);
   x+=xSpeed;
   y+=ySpeed+gravity;
   if(x<0 || x>canvas.width) {xSpeed = -xSpeed}
   if(y<0 || y>canvas.height) {ySpeed = -ySpeed}
-
+  platform();
 }
 
 function setDirection(dir) {
-  if(dir =="up") {
-    xSpeed = 0;
-    ySpeed = -5;
+  if(dir =="jump" && "jump") {
+    y-= 100;
   }
-  if(dir =="down") {
-    xSpeed = 0;
-    ySpeed = 5;
+  if(dir =="jump") {
+    y-= 100;
   }
   if(dir =="right") {
     xSpeed = -5;
@@ -51,10 +51,10 @@ function setDirection(dir) {
 
 var keyActions = {
   16: "stop",
-  39: "left",
-  38: "up",
-  37: "right",
   40: "down",
+  39: "left",
+  38: "jump",
+  37: "right",
 };
   
 document.addEventListener('keydown',function(event) {
@@ -67,20 +67,35 @@ document.addEventListener('keyup',function(event) {
   setDirection("stop");
 });
  
-  
+/*
 function platform() {
-  cx.fillstyle = "grey";
-  cx.fillRect(5,500,100,10);
-  if (y==500)  {
-  gravity = 0;
+  cx.fillStyle="grey";
+  cx.fillRect(30,500,100,10);
+  cx.fillRect(230,400,100,10);
+  if (y==395 && x<80)  {
+    gravity = 0;
   }
-  else {
-    gravity = 5;
-  }
+    
+  }//  else {gravity = 5;}
 }
+*/
+var plat=[];
+plat.push({x:0, y:435, w:100, h:10});
+plat.push({x:750, y:850, w:120, h:10});
+plat.push({x:600, y:500, w:350, h:30});
+plat.push({x:450, y:395, w:100, h:10});
+plat.push({x:350, y:425, w:150, h:15});
+plat.push({x:0, y:730, w:1300, h:30});
 
 
-
-
+function platform() {
+  gravity = 5;
+  cx.fillStyle="blue";
+  for (var i =0; i<plat.length; i++) {
+    cx.fillRect(plat[i].x, plat[i].y, plat[i].w, plat[i].h ) ;
+    if (y==plat[i].y-playerH &&
+    x>plat[i].x-playerW &&
+    x<plat[i].x+plat[i].w)
+    {gravity=0}}}
 
 animate();
