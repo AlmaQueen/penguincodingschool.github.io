e6bac20ac081828a00ed778a806b1ae6e39f3721
studentpenguincoding <student@penguincodingschool.com>
2017-07-25 17:56:29 -0400

    Add files via upload

--- penguin114/game.js
+++ penguin114/game.js
@@ -1,20 +1,17 @@
-/*This is game is still in very early development.*/
+/*This is game is still in development.*/
 /*Don't expect much of it.*/
 /*New features might be added at any time.*/
-/*It has glitches.*/
-
 
 
 var canvas = document.getElementById("canvas");
 var cx = canvas.getContext("2d");
 canvas.width=1000;
-canvas.height=805;
+canvas.height=800;
 var img_player = document.createElement("img");
-img_player.src = "feather.jpg";
-
+img_player.src = "cheese.png";
 
 var x = 0;
-var y= 0 ;
+var y= 0;
 var playerW = 150;
 var playerH = 100;
 var xSpeed = 0;
@@ -30,6 +27,11 @@ var xMonsterSpeed = -5;
 var yMonsterSpeed = 0;
 var monsterW = 50; //monster width
 var monsterH = 50; //monster height
+var xC = 500;
+var yC = 300;
+var wC = 20;
+var hC = 20;
+var score =0;
 
 function animate() {
 req=requestAnimationFrame(animate);
@@ -37,10 +39,11 @@ cx.clearRect(0,0,canvas.width, canvas.height);
 cx.drawImage(img_player, x,y, playerW, playerH);
 x+=xSpeed;
 y+=ySpeed+gravity;
-counterDisplay();
 platform();
 obstacle();
 monster();
+coin();
+scoreDisplay();
 if (x <0 || x> canvas.width-playerW) {
 xSpeed=-xSpeed;
 
@@ -54,12 +57,6 @@ ySpeed=-ySpeed;
 if (x>800) {
   gameWin();
 }
-
-}
-
-function counterDisplay() {
-  cx.font = "325px";
-  cx.fillText('Platform Touches:'+counter,100,100);
 
 }
 
@@ -121,19 +118,14 @@ plat.push({x: 0, y:785, w:canvas.width, h:10});
 
 function platform() {
 gravity =5;
+for(var i = 0; i<plat.length; i++) {
 cx.fillStyle="grey";
-for (var i = 0; i<plat.length; i++) {
- cx.fillRect(plat[i].x, plat[i].y,plat[i].w, plat[i].h);
+cx.fillRect(plat[i].x, plat[i].y, plat[i].w, plat[i].h)
  if (y===plat[i].y-playerH &&
     x>=plat[i].x-playerW &&
     x<=plat[i].x + plat[i].w)
   {gravity=0}
-else {}
-
-
 }
-
-
 }
 
 lava.push({x:400, y:600, w:500, h:10});
@@ -156,15 +148,15 @@ function stop() {
   }};
 
 function gameOver() {
-  cx.fillSytle = "Red";
-  cx.font = "30px Didot";
+  cx.fillStyle = "Red";
+  cx.font = "30px Comic Sans MS";
   cx.fillText("Game Over", 10,50);
   stop();
 }
 
 function gameWin() {
   cx.fillStyle = "Green";
-  cx.font = "30px Didot";
+  cx.font = "30px Comic Sans MS";
   cx.fillText("You Won!",10,50);
   stop();
 }
@@ -174,27 +166,67 @@ cx.fillStyle = "blue";
 cx.fillRect(xMonster,yMonster, monsterW,monsterH);
 xMonster += xMonsterSpeed;
 yMonster += yMonsterSpeed;
-if (xMonster===x && yMonster ===y)  {
-    gameOver();
-} else if (xMonster<0||xMonster>1000) {
+if(x+playerW > xMonster && xMonster+monsterW >x && yMonster+monsterH > y && y+playerH > yMonster) {
+  gameOver();
+}
+if (xMonster<0||xMonster>1000) {
     xMonsterSpeed = -xMonsterSpeed;
 }
 }
-if(x+playerW > xMonster && xMonster+monsterW >x && yMonster+monsterH > y && y+playerH > yMonster) {
-  gameOver();
+
+function coin() {
+ cx.fillStyle = "gold"
+ cx.fillRect(xC,yC,wC, hC)
+ if(x+playerW > xC &&
+ xC + wC> x &&
+ yC + hC >y &&
+ y+playerH >yC)
+ {
+   score+=10;
+   xC =Math.random()*canvas.width;
+   yC = Math.random()*canvas.height;
+}
+}
+ /*
+ if (x+playerW > xC && xC + wC> x && yC+hC >y && y+playerH >yc ) {
+   var i = Math.ceil(Math.random()*plat.length);
+   xC = plat[i].x;
+   yC = plat[i].y;
+   
+ }
+
+if (x+playerW > xC && xC + wC> x && yC+hC >y && y+playerH >yC )
+{
+   score+= 10;
+}
+ 
+function coin()
+  cx.drawImage(img_coin,xC, yC, wC, hC);
+  if (x+playerW > xC && xC+wC >x && yC+hCy && y+playerH>yC)
+  {
+  score +=10;
+  var i = getRandomInt(1,plat.length);
+    xC = plat[i].x;
+    yC = plat[i].y-40;
+//  xC = Math.random()*(canvas.width-1000);
+ // yC = getRandomInt(1,3)*150;
+  }
+  
   
 }
+*/
 
-  
+function getRandomInt(min,max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random()*(max-min))+min;
+}
 
-
-
-
-
-
-
-  
-
+function scoreDisplay() {
+  cx.fillStyle ="Green";
+  cx.font = "30px Lobster";
+  cx.fillText("Score: "+score,500,100);
+}
 
 
 
@@ -203,97 +235,3 @@ if(x+playerW > xMonster && xMonster+monsterW >x && yMonster+monsterH > y && y+pl
 
 
 animate();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
