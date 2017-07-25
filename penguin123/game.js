533fa00147494b20758ce6b7ac07993cae8c9d61
studentpenguincoding <student@penguincodingschool.com>
2017-07-25 14:47:06 -0400

    Add files via upload

--- penguin123/game.js
+++ penguin123/game.js
@@ -1,3 +1,4 @@
+
 var canvas = document.getElementById("canvas");
 var cx = canvas.getContext("2d");
 canvas.width = 1000;
@@ -6,29 +7,31 @@ canvas.height = 700;
 var img_player = document.createElement("img");
 img_player.src = "Player.png";
 
-var x = 500;
+var x = 0;
 var y = 0;
-var playerw = 250;
-var playerh = 250;
-var xSpeed = -5;
-var ySpeed = -5;
+var playerw = 100;
+var playerh = 100;
+var xSpeed = 0;
+var ySpeed = 0;
+var gravity=5;
 
 function animate() {
   requestAnimationFrame(animate);
   cx.clearRect(0,0,canvas.width, canvas.height);
   cx.drawImage(img_player,x,y, playerw,playerh);
   x+=xSpeed;
-  y+=ySpeed;
+  y+=ySpeed+gravity;
   
-  if (x<0 || x>canvas.width) {xSpeed = -xSpeed}; //The equal sign means that you are assigning one value to another...
-  if (y<0 || y>canvas.height) {ySpeed = -ySpeed};
+  if (x<0 || x>canvas.width) {xSpeed = -xSpeed} //The equal sign means that you are assigning one value to another...
+  if (y<0 || y>canvas.height) {ySpeed = -ySpeed}
+ platform();
+  
 }
 
 function setDirection(dir) {
-  
- if(dir =="up"){
+ if(dir =="jump"){
   xSpeed = 0;
-  ySpeed = -5;
+  y-=200;
 }
   if(dir =="down"){
   xSpeed = 0;
@@ -38,7 +41,7 @@ function setDirection(dir) {
   xSpeed = 5;
   ySpeed = 0;
 }
-  if(dir =="left"){
+  if(dir =="left" && x>0){
   xSpeed = -5;
   ySpeed = 0;
 }
@@ -46,16 +49,15 @@ if(dir =="stop"){
   xSpeed = 0;
   ySpeed = 0;
 }
-    
 }
 
 
 var keyActions= {
-  32:"stop",
+  32:"jump",
   37:"left",
-  38:"up",
+  //38:"up",
   39:"right",
-  40:"down"
+  //40:"down"
 };
 
 document.addEventListener('keydown',function(event)
@@ -71,6 +73,42 @@ document.addEventListener('keyup',function(event)
 });
 
 
+var plat=[];
+plat.push({x:0, y:500, w:100, h:10});
+plat.push({x:300, y:400, w:100, h:10});
+plat.push({x:300, y:100, w:100, h:10});
+plat.push({x:500, y:200, w:100, h:10});
+plat.push({x:600, y:500, w:100, h:10});
+plat.push({x:800, y:200, w:100, h:10});
+plat.push({x:900, y:700, w:100, h:10});
+plat.push({x:970, y:300, w:100, h:10});
+plat.push({x:490, y:700, w:100, h:10});
+plat.push({x:170, y:200, w:100, h:10});
+plat.push({x:200, y:650, w:1000, h:10});
+plat.push({x:750, y:300, w:100, h:10});
+plat.push({x:500, y:700, w:100, h:10});
+plat.push({x:190, y:300, w:100, h:10});
+plat.push({x:900, y:60, w:100, h:10});
+plat.push({x:970, y:400, w:100, h:10});
+plat.push({x:490, y:700, w:100, h:10});
+plat.push({x:0, y:650, w:1000, h:10});
 
+
+function platform() {
+  gravity=5;
+  cx.fillStyle="#48F315";
+  
+  for (var i=0; i<plat.length; i++) {
+    cx.fillRect
+    (plat[i].x, plat[i].y, plat[i].w, plat[i].h);
+    if (y==plat[i].y-playerh &&
+    x>=plat[i].x-playerw &&
+    x<plat[i].x+plat[i].w)
+    {gravity=0}
+  }
+  if (y==plat[i].y+plat[i].w) {
+    ySpeed=0;
+  }
+}
 
 animate();
