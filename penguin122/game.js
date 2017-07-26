9709ca9e66ab492058fdba7ef33b658c1ea8b049
studentpenguincoding <student@penguincodingschool.com>
2017-07-26 11:20:02 -0400

    Add files via upload

--- penguin122/game.js
+++ penguin122/game.js
@@ -15,20 +15,27 @@ var yspeed=0;
 var gravity = 5;
 
 function animate() {
-    requestAnimationFrame(animate);
+    req =requestAnimationFrame(animate);
     cx.clearRect(0,0,canvas.width, canvas.height);
     cx.drawImage(img_player,x,y,playerw,playerh);
     x+=xspeed;
     y+=yspeed + gravity;
   if(x<0 || x>canvas.width-100) {xspeed=-xspeed}
-  if (y<0|| y>canvas.height-100) {yspeed=-yspeed}
+  if (y>canvas.height-playerh) {life-=1;
+  x=0;
+  y=0;
+  if(life==0) {gameover()}}
   platform();
+  obstacle();
+  sinking();
+  rising();
+  lifeleft();
 }
 function setDirection(dir){
   if(dir=="jump" && gravity===0){
       y-=250;
   }
-if(dir=="down" && y<510){
+if(dir=="down" && y<canvas.height){
       xspeed=0;
       yspeed=5;
 }
@@ -65,30 +72,30 @@ document.addEventListener('keyup', function(event){
   setDirection("stop");
 });
 
-/*function platform(){
+function platform(){
   cx.fillStyle="blue";
   cx.fillRect(20,500,100,10);
   if(y==500-playerh && x<100 && x> 20- playerw)
     {gravity=0}
    else {gravity= 5}
 }
-*/
+
 var plat=[];
-plat.push({x:0,y:500,w:200,h:10})
-plat.push({x:75,y:200,w:150,h:10})
-plat.push({x:450,y:570,w:50,h:10})
-plat.push({x:200,y:400,w:130,h:10})
-plat.push({x:504,y:50,w:100,h:10})
-plat.push({x:800,y:220,w:100,h:10})
-plat.push({x:650,y:500,w:100,h:10})
-plat.push({x:500,y:400,w:100,h:10})
-plat.push({x:200,y:600,w:100,h:10})
-plat.push({x:0,y:600,w:1000,h:20})
-plat.push({x:650,y:300,w:100,h:10})
+plat.push({x:0,y:500,w:200,h:10});
+plat.push({x:75,y:200,w:150,h:10});
+plat.push({x:450,y:570,w:50,h:10});
+plat.push({x:200,y:400,w:130,h:10});
+plat.push({x:504,y:50,w:100,h:10});
+plat.push({x:800,y:210,w:100,h:10});
+plat.push({x:650,y:500,w:100,h:10});
+plat.push({x:500,y:400,w:100,h:10});
+plat.push({x:200,y:600,w:100,h:10});
+plat.push({x:100,y:600,w:1000,h:20});
+plat.push({x:650,y:300,w:100,h:10});
 
 function platform() {
-  gravity=5
-  cx.fillStyle="white"
+  gravity=5;
+  cx.fillStyle="white";
   for ( var i =0 ; i<plat.length;i++){
     cx. fillRect(plat[i].x, plat[i].y, plat[i].w, plat[i].h);
     if (y==plat[i].y-playerh&&
@@ -97,6 +104,56 @@ function platform() {
     {gravity=0}
   }
 }
+
+var life=5;
+
+function obstacle() {
+  cx.fillStyle="red";
+  for ( var i =0 ; i<lava.length;i++){
+    cx. fillRect(lava[i].x, lava[i].y, lava[i].w, lava[i].h);
+    if (y==lava[i].y-playerh&&
+    x>lava[i].x-playerw &&
+    x<lava[i].x+lava[i].w)
+    {life-=1;
+    x=5;
+    y=5;
+    if(life===0) {gameover()}
+    }
+  }
+}
+
+function lifeleft(){
+  cx.fillStyle= "green";
+  cx.font= "30px Impact";
+  cx.fillText("Lives "+life,100,50);
+}
+
+function gameover() {
+  cx.fillStyle="orange";
+  cx.font ="50px Impact";
+  cx.fillText("Game Over",500,50)
+  stop();
+}
+
+function stop() {
+  if(req) {
+    cancelAnimationFrame(req);
+    req =undefined;
+  }
+}
+
+var lava=[];
+lava.push({x:0,y:600,w:1000,h:200});
+function rising() {
+  lava[0].y-=50;
+}
+setTimeout(sinking(), 5000);
+
+function sinking() {
+  lava[0].y+=50;
+
+}
+
 
 animate();
 
