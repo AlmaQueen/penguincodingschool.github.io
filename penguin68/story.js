a0f55474bf81f13b49e02271fc1662947f6c67fd
studentpenguincoding <student@penguincodingschool.com>
2017-07-27 14:58:09 -0700

    Add files via upload

--- penguin68/story.js
+++ penguin68/story.js
@@ -1,5 +1,5 @@
 function question1() {
-    var answer = prompt("would you like anything to drink or eat?");
+    var answer = prompt("What would you like to drink or eat?");
     if(answer =="water") {
       var wateranswer = prompt("Sparkling or still?");
       if(wateranswer =="still water") { alert("Sure!  I will get you a water.")}
@@ -7,13 +7,15 @@ function question1() {
       alert("Ok, I will get you a sparkling water!")  }
     else {alert("coming right up!")}
     }
-    /*else if (answer=="no")
+    /*elif (answer=="no")
     alert("Ok, Are you sure?")
-    else if ("yes I am sure")
+    elif ("yes I am sure")
     alert("Ok, nothing it is!")
-    else if(answer=="nothing") {alert("Ok, Are you sure?")}
+    elif(answer=="nothing") {alert("Ok, Are you sure?")}
     */
     else
     {alert("Coming right up")}
     
 }
+
+elif
