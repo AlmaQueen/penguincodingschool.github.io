614e07ec3ba61f3677625ab0b674fe4b056b1ccc
studentpenguincoding <student@penguincodingschool.com>
2017-07-27 08:00:08 -0700

    Add files via upload

--- penguin123/game.js
+++ penguin123/game.js
@@ -15,6 +15,12 @@ var xSpeed = 0;
 var ySpeed = 0;
 var gravity=5;
 var life=10;
+var xM = 800;
+var yM = 180
+var xMsp = 10;
+var yMsp = 10;
+var mW = 100;
+var mH = 100;
 
 function animate() {
   req=requestAnimationFrame(animate);
@@ -28,10 +34,13 @@ function animate() {
  platform();
  obstacle();
  lifeleft();
+ monster();
+ coin();
+scoreDisplay();
  }
 
 function setDirection(dir) {
- if(dir =="jump"){
+ if(dir =="jump" && y>0){
   xSpeed = 0;
   y-=200;
 }
@@ -82,18 +91,18 @@ plat.push({x:300, y:100, w:100, h:10});
 plat.push({x:500, y:200, w:100, h:10});
 plat.push({x:600, y:500, w:100, h:10});
 plat.push({x:800, y:200, w:100, h:10});
-plat.push({x:900, y:700, w:100, h:10});
+//plat.push({x:900, y:700, w:100, h:10});
 plat.push({x:970, y:300, w:100, h:10});
-plat.push({x:490, y:700, w:100, h:10});
+//plat.push({x:490, y:700, w:100, h:10});
 plat.push({x:170, y:200, w:100, h:10});
-plat.push({x:200, y:650, w:1000, h:10});
+//plat.push({x:200, y:650, w:1000, h:10});
 plat.push({x:750, y:300, w:100, h:10});
-plat.push({x:500, y:700, w:100, h:10});
+//plat.push({x:500, y:700, w:100, h:10});
 plat.push({x:190, y:300, w:100, h:10});
 plat.push({x:900, y:60, w:100, h:10});
 plat.push({x:970, y:400, w:100, h:10});
-plat.push({x:490, y:700, w:100, h:10});
-plat.push({x:0, y:650, w:1000, h:10});
+//plat.push({x:490, y:700, w:100, h:10});
+//plat.push({x:0, y:650, w:1000, h:10});
 
 
 function platform() {
@@ -107,16 +116,17 @@ function platform() {
     x>=plat[i].x-playerw &&
     x<plat[i].x+plat[i].w)
     {gravity=0}
-    /*
+    
+  }
+}
+ function platform2() {
+   
     if(x+playerw>plat[i].x &&
   x<plat[i].x+plat[i].w &&
   y<=plat[i].y+plat[i].h &&
   y>plat[i].y)
   {ySpeed=-ySpeed}
-  */
-  }
-  
-  
+
 }
 
 var lava=[];
@@ -140,14 +150,14 @@ function obstacle() {
 }
 function lifeleft (){
   cx.fillStyle="turquoise";
-  cx.font="30px Comic Sans MS";
-  cx.fillText("Lives Left:" +life, 100, 50);
+  cx.font="40px Comic Sans MS";
+  cx.fillText("Lives Left:" +life, 30, 50);
   }
   
 function gameover (){
-  cx.fillStyle="black";
-  cx.font="30px Comic Sans MS";
-  cx.fillText("Game Over", 100, 50);
+  cx.fillStyle="red";
+  cx.font="100px Comic Sans MS";
+  cx.fillText("GameOver", 500, 350);
   stop();
   }
   
@@ -158,9 +168,65 @@ function gameover (){
     }
 }
   
+var img_monster = document.createElement("img");
+img_monster.src = "http://tf2classic.com/wiki/images/thumb/2/2f/PoacherPride3rdperson.png/120px-PoacherPride3rdperson.png";
 
-    
+function monster () {
+  cx.drawImage(img_monster, xM, yM, mW, mH);
+  xM+=xMsp;
+  yM+=yMsp;
+  if (x+playerw>xM && xM+mW>x && yM+mH>y && y+playerh>yM)
+  {life -=1;
+  x=0; y=0;
+    if(life===0){gameover()}
+  }
+  if (xM<0 || xM>canvas.width-mW) {
+    xMsp = -xMsp;
+  } if(yM<0 || yM>canvas.height-mH) {
+    yMsp = -yMsp;
+  }
+  
+}
 
 
+var xC = 80;
+var yC = 450;
+var wC = 60;
+var hC = 60;
+var score = 0;
+var img_coin = document.createElement("img");
+img_coin.src = "http://www.pngmart.com/files/3/Lakshmi-Gold-Coin-PNG-File.png";
+
+function coin() {
+  cx.drawImage(img_coin, xC, yC, wC, hC);
+  if (x+playerw>xC && xC+wC>x && yC+hC>y && y+playerh>yC)
+  
+  {score+=10;
+  var i = Math.floor(Math.random()*plat.length);
+  xC = plat[i].x;
+  yC = plat[i].y-50;
+}
+
+}
+
+function scoreDisplay(){
+  cx.fillStyle="pink";
+    cx.font="40px Comic Sans MS";
+  cx.fillText("Score: "+score, 300, 50);
+}
+
+/*
+var xCC = Math.random()*canvas.width;
+var yCC = Math.random()*canvas.height;
+
+for(var i = 0; i<lava.length; i++) {
+  if(xCC < lava[i].x+lava[i].w && xCC >= lava[i].x && yCC > lava[i].y && yCC < lava[i].y+lava[i].h) {xC=xCC+100; yC=yCC+100}
+  else {
+    xC = xCC;
+    yC = yCC;
+  }
+}
+
+*/
 
 animate();
