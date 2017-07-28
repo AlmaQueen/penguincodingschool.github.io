5f30af523b9bddad5a0fff2f532c7dff9e940393
studentpenguincoding <student@penguincodingschool.com>
2017-07-28 11:04:41 -0400

    Add files via upload

--- penguin119/game.js
+++ penguin119/game.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById("canvas");
 var cx = canvas.getContext("2d");
-canvas.width=1200;
-canvas.height=700;
+canvas.width=1350;
+canvas.height=900;
 
 var img_player = document.createElement ("img");
 img_player.src = "lollipop.png";
@@ -32,6 +32,10 @@ platform();
 obstacle();
 monster();
 displayLife();
+coin();
+scoreDisplay();
+bonus();
+
 }
 
 function setDirection(dir) {
@@ -81,14 +85,21 @@ var plat=[];
 plat.push({x:0,y:100,w:100,h:10})
 plat.push({x:700, y:150, w:100, h:10})
 plat.push({x:150, y:400, w:100, h:10})
-plat.push({x:300, y:600, w:100, h:10})
+plat.push({x:1000, y:600, w:100, h:10})
 plat.push({x:605, y:250, w:100, h:10})
 plat.push({x:700, y:500, w:100, h:10})
 plat.push({x:975, y:700, w:100, h:10})
+plat.push({x:450, y:375, w:100, h:10})
+plat.push({x:900, y:600, w:100, h:10})
+plat.push({x:1150, y:700, w:100, h:10})
+plat.push({x:1200, y:300, w:100, h:10})
+plat.push({x:800, y:350, w:100, h:10})
+plat.push({x:300, y:250, w:100, h:10})
+plat.push({x:1150, y:100, w:100, h:10})
 
 function platform () {
   gravity =5;
-  cx.fillStyle="transparent";
+  cx.fillStyle="white";
   for (var i =0; i<plat.length; i++) {
     cx.fillRect(plat[i].x, plat[i].y, plat[i].w, plat[i].h);
     if (y==plat[i].y-playerH &&
@@ -99,7 +110,7 @@ function platform () {
 }
 
 var lava = []
-lava.push({x:0,y:650,w:1200,h:10})
+lava.push({x:0,y:canvas.height,w:1200,h:10})
 
 var life = 9;
 
@@ -140,22 +151,32 @@ img_m4.src = "monster.png";
 img_m5.src = "monster.png";
  
 var mr =[];
-mr.push({img:img_m1, x:400, y:300, w:50, h:50, msp:-5})
-mr.push({img:img_m2, x:500, y:280, w:50, h:50, msp:5})
-mr.push({img:img_m3, x:600, y:290, w:50, h:50, msp:-5})
-mr.push({img:img_m4, x:700, y:310, w:50, h:50, msp:2})
-mr.push({img:img_m5, x:900, y:320, w:50, h:50, msp:-2})
+//mr.push({img:img_m1, x:400, y:300, w:50, h:50, mspx:7, mspy:-2})
+mr.push({img:img_m2, x:500, y:300, w:50, h:50, mspx:10, mspy:-3})
+//mr.push({img:img_m3, x:600, y:290, w:50, h:50, mspx:-5, mspx:-7})
+//mr.push({img:img_m4, x:700, y:310, w:50, h:50, mspx:2, mspy:3})
+//mr.push({img:img_m5, x:900, y:320, w:50, h:50, mspx:-2, mspy:-10})
 
- var mx1 = 400;
- var my1 = 300;
- var mxsp = 5;
- var mxsp2 = 5;
- 
+
  function monster() {
-  for (var i =2; i<mr.length; i++) {
-  //cx.drawImage(mr[i].img,mr[i].x,mr[i].y,mr[i].w,mr[i].h);
-  mr[i].y+=mr[i].msp;
+  for (var i =0; i<mr.length; i++) {
+  cx.drawImage(mr[i].img,mr[i].x,mr[i].y,mr[i].w,mr[i].h);
+  mr[i].y+=mr[i].mspy;
+  mr[i].x+=mr[i].mspx;
+  if(mr[i].y > canvas.height -mr[i].h|| mr[i].y<0){
+    mr[i].mspy=-mr[i].mspy
+  }
+  if(mr[i].x > canvas.width || mr[i].x<0){
+    mr[i].mspx=-mr[i].mspx;
+  }
+  if(x+playerW > mr[i].x && mr[i].x+mr[i].w >x && mr[i].y+mr[i].h > y && y+playerH > mr[i].y)
+ {life--;y=0; x=0;
+   if(life===0){gameOver()}
+ }
   
+  }
+ }
+  /*
   if(x+playerW > mr[i].x && mr[i].x+mr[i].w >x && mr[i].y+mr[i].h > y && y+playerH > mr[i].y)
  {
   life--;
@@ -170,13 +191,13 @@ mr.push({img:img_m5, x:900, y:320, w:50, h:50, msp:-2})
   mr[0].y+=mr[0].msp;
   mr[0].x+=mxsp;
   
-  if(mr[0].y > canvas.height || mr[0].y<0){
+  if(mr[0].y > canvas.height -mr[0].h|| mr[0].y<0){
     mr[0].msp=-mr[0].msp
   }
   if(mr[0].x > canvas.width || mr[0].x<0){
     mxsp=-mxsp
   }
-  if(mr[1].y > canvas.height || mr[1].y<1){
+  if(mr[1].y > canvas.height-mr[0].h || mr[1].y<1){
     mr[1].msp=-mr[1].msp
   }
   if(mr[1].x > canvas.width || mr[1].x<1){
@@ -186,37 +207,82 @@ mr.push({img:img_m5, x:900, y:320, w:50, h:50, msp:-2})
   mr[1].y+=mr[1].msp;
      mr[1].x+=mxsp2;
  }
-    
-  var xC = 80;
-  var yC = 450;
+  */
+  var xC = 150;
+  var yC = 300;
   var wC = 30;
   var hC = 30;
   
-  //function coin() {
-    //cx.drawImage(img_coin xC,yC,wC,hC)
+  function coin() {
+    cx.drawImage(img_coin, xC,yC,wC,hC);
+    if (x+playerW > xC && xC + wC> x && yC + hC >y && y+playerH >yC)
+    {score+=15;
+    var audio = new Audio('coin.wav');
+    var coinsound = new Audio('coin.wav')
+    audio.play();
     
-//  }
-  
+        if (score>=300) {gameWin()}
 
+    var i = Math.floor(Math.random()*plat.length);
+    xC = plat[i].x;
+    yC = plat[i].y - 40;}
+     }
+  function gameWin() {
+    cx.fillStyle = "white";
+    cx.font = "30px Comic Sans MS";
+    cx.fillText("You Won!",10,50);
+    stop();
+  }
+  
+var score = 0;
+var img_coin=document.createElement("img");
+img_coin.src="coin.png"
 
   function gameOver() {
-    cx.fillStyle = "black";
+    cx.fillStyle = "white";
     cx. font = "30px Comic Sans MS";
     cx.fillText("Game Over",10,50);
     stop();
   }
+ 
+ function scoreDisplay() {
+   cx.fillStyle = "white";
+   cx.font = "30px Comic Sans MS";
+   cx.fillText("Score: "+score,700,50)
+ }
   
   function displayLife() {
     cx.fillStyle = "white";
     cx.font = "30px Comic Sans MS";
-    cx.fillText("Lives Left: "+life, 100,50)
+    cx.fillText("Lives Left: "+life, 500,50)
+  
   }
+  
+  if (x+playerW > vM && vM+vW > x)
+  if (aM+aH > y && y+playerH > aM)
+  {score+=45
+  
+  
+  var boost = Math.ceil(Math.random()*17);
+  
+  
+  
+  var audio = new Audio('coin.wav');
   
   function stop() {
     if(req) {
       cancelAnimationFrame(req);
       req = undefined;
   }}
-
+  
+function bonus() {
+  if(score==boost*15){
+   //  var i = Math.floor(Math.random()*plat.length);
+    //vM = plat[i].x;
+    //aM = plat[i].y - 40;
+  vM=300;
+  aM=300;
+  }
+}
 
 animate();
