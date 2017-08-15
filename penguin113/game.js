c0e2ec1d0b2b2a4374e5d98ef66a5963f907ca96
studentpenguincoding <student@penguincodingschool.com>
2017-08-15 18:01:40 -0400

    Add files via upload

--- penguin113/game.js
+++ penguin113/game.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById("canvas");
 var cx = canvas.getContext ("2d");
-canvas.width=1500;
+canvas.width=1100;
 canvas.height=700;
 
 var img_player = document.createElement("img");
@@ -23,15 +23,15 @@ x+=xSpeed;
 y+=ySpeed+gravity;
 platform();
 obstacle();
+monster();
+coin();
  if (x <0 || x>canvas.width-playerW) {
    xSpeed=-xSpeed;
  }
  if (y <0 ||y >canvas.height-playerH){
     ySpeed=-ySpeed;
  }
- if (x>800) {
-   gameWin();
-   }
+ 
  }
 
 function stop() {
@@ -112,7 +112,11 @@ plat.push({x: 200, y: 60, w: 100, h: 10});
 plat.push({x: 300, y: 40, w: 100, h: 10});
 plat.push({x: 400, y: 90, w: 125, h: 10});
 plat.push({x: 500, y: 70, w: 150, h: 10});
-plat.push({x: 600,y: 80,w: 200,h: 10});
+plat.push({x: 600,y: 80, w: 200, h: 10});
+plat.push({x: 700, y: 110,w: 210, h: 10});
+plat.push({x: 900, y: 180,w: 210, h: 10});
+plat.push({x: 1100, y: 160,w: 125, h: 10});
+plat.push({x: 1200, y: 145,w: 250, h: 10});
 plat.push({x: 0, y: 690, w:canvas.width, h: 10});
 
 
@@ -135,7 +139,7 @@ lava.push({x: 100, y: 100, w:10, h:10});
 
 function gameOver() {
   cx.fillStyle ="Red";
-  cx.font = "50px Impact";
+  cx.font = "30px Impact";
   cx.fillText("Game Over", 10,50);
   stop();
 }
@@ -146,6 +150,64 @@ function gameWin() {
   cx.fillText = ("You Won!",10,50);
   stop();
 }
+
+var xMonster = 500;
+var yMonster = 0;
+
+var xMonsterSpeed = -5;
+var yMonsterSpeed =0;
+
+var MonsterW = 50; //monster width
+var MonsterH = 50; //monster height
+
+var img_monster = document.createElement("img");
+img_monster.src = "car.jpg";
+
+
+function monster() {
+  //cx.fillStyle = "blue";
+  //cx.fillRectd(xMonster,yMonster, MonsterW,MonsterH);
+  cx.drawImage(img_monster,xMonster,yMonster, MonsterW,MonsterH)
+  xMonster += xMonsterSpeed;
+  yMonster += yMonsterSpeed;
+  if (x+playerW > xMonster && xMonster+MonsterW >x && yMonster+MonsterH > y && y+playerH > yMonster) {
+      gameOver();
+  }
+  if (xMonster<10 || xMonster>canvas.width) {
+      xMonsterSpeed = -xMonsterSpeed;
+   }
+}
+
+var xC = 70;
+var yC = 60;
+var wC = 20;
+var hC = 20;
+
+
+var img_coin = document.createElement("img");
+img_coin.src = "coin.png";
+
+function coin() {
+  cx.drawImage(img_coin,xC,yC,wC,hC);
+  if(x+playerW > xC && xC + wC> x && yC+hC >y && y+player >yC) {
+    score=+10;
+  var i = Math.ceil(Math.random()*plat.length);
+  //xC = plat[i].x;
+  //yC = plat[i].y - 40;
+  xC = Math.random()*(canvas.width - 100);
+  yC = getRandomInt(1,3) * 150;
+  
+}
+}
+
+function getRandomInt(min,max){
+  min = Math.ceil(min);
+  max = Math.ceil(max);
+  return Math.floor(Math.random()*(max-min)) + min;
+}
+
+var score = 0
+
 
 
 animate();
